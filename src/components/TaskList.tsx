588eb69670fa0ef168a6c14cc53024dab5d6850a
Victor Menegazzo <victor.umenegazzo@gmail.com>
2021-03-09 12:02:16 -0300

    :sparkles: feat: handle tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return;
+
+    const task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldState => [...oldState, task])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const task = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(task)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const index = tasks.filter(task => task.id !== id);
+
+    setTasks(index)
   }
 
   return (
